851401260c43b0b5b2e895118f53a26ed2d169cd
chandan-gupta96 <chandanavaquantian@gmail.com>
2021-07-15 16:21:27 +0530

    initializedd new project

---
+++ js/main.js
@@ -0,0 +1,25 @@
+const breakpoint = [{
+    breakpoint: 1280,
+    settings:{
+        slidesToShow: 4
+    }
+},
+{
+    breakpoint: 1009,
+    settings:{
+        slidesToShow: 3
+    }
+},
+{
+    breakpoint: 720,
+    settings:{
+        slidesToShow: 2
+    }
+},
+{
+    breakpoint: 460,
+    settings:{
+        slidesToShow: 1
+    }
+}
+];
